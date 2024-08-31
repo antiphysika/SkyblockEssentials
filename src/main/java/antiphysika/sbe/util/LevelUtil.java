615ecfb7090b96f4a792d7a458d9fd12fa7f3235
Francis <antiphysika@gmail.com>
2024-08-31 13:35:16 +0100

    Remove static field `LOGGER` from `LevelUtil`

--- src/main/java/antiphysika/sbe/util/LevelUtil.java
+++ src/main/java/antiphysika/sbe/util/LevelUtil.java
@@ -4,17 +4,13 @@
 
 package antiphysika.sbe.util;
 
-import antiphysika.sbe.SkyblockEssentials;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.slf4j.Logger;
 
 public class LevelUtil
 {
-  private static final Logger LOGGER = SkyblockEssentials.getLogger();
-
   public static void spawnItemStack (Level level, Player player, ItemStack itemStack)
   {
     level.addFreshEntity(
